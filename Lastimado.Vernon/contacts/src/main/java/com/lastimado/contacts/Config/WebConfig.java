7e4de61a69b7ac28dd9fcdd61672079becc272e3
LAB14_USER ON WS17 <L14X18W17@citulab.net>
2025-03-10 18:58:51 +0800

    Contacts Added

---
+++ Lastimado.Vernon/contacts/src/main/java/com/lastimado/contacts/Config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.lastimado.contacts.Config;
+
+public class WebConfig {
+}
---
+++ Lastimado.Vernon/contacts/src/test/java/com/lastimado/contacts/ContactsApplication.java
@@ -0,0 +1,13 @@
+package com.lastimado.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
